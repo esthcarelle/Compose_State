9315ac0c625cc4a4ec1288cfa941f4d00b52a9d3
carrelle <esthcarelle@gmail.com>
2023-10-23 10:45:57 +0200

    Added conditions to the UI composables

--- app/src/main/java/com/catchy/composestate/ui/WaterCounter.kt
+++ app/src/main/java/com/catchy/composestate/ui/WaterCounter.kt
@@ -4,22 +4,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count: MutableState<Int> = remember { mutableStateOf(0) }
+    var count by remember { mutableStateOf(0) }
 
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+    if (count > 0) {
+        // This text is present if the button has been clicked
+        // at least once; absent otherwise
+        Text(
+            text = "You've had $count glasses.",
+            modifier = modifier.padding(16.dp)
+        )
+    }
 
-    Button(onClick = { count.value++ },modifier = modifier.padding(8.dp)) {
+    Button(onClick = { count++ },modifier = modifier.padding(8.dp),enabled = count < 10) {
         Text("Add one")
     }
 }
