aedd56caf6dde303e326a0d1c0e36461be203da7
carrelle <esthcarelle@gmail.com>
2023-10-24 17:44:33 +0200

    Added state hosting to composables

--- app/src/main/java/com/catchy/composestate/ui/WellnessScreen.kt
+++ app/src/main/java/com/catchy/composestate/ui/WellnessScreen.kt
@@ -1,9 +1,18 @@
 package com.catchy.composestate.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    StatefulCounter(modifier)
+}
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count, { count++ }, modifier)
 }
