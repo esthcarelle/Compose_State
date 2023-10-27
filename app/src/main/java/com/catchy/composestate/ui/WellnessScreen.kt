8e832accb0326699f1a9d37ef61f891c74a4236f
carrelle <esthcarelle@gmail.com>
2023-10-27 11:46:11 +0200

    Made the WellnessTask List hoisted to the main screen

--- app/src/main/java/com/catchy/composestate/ui/WellnessScreen.kt
+++ app/src/main/java/com/catchy/composestate/ui/WellnessScreen.kt
@@ -1,10 +1,12 @@
 package com.catchy.composestate.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -13,10 +15,11 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 }
 @Composable
 fun StatefulCounter(modifier: Modifier = Modifier) {
-    var waterCount by remember { mutableStateOf(0) }
+    Column(modifier = modifier) {
+        StatefulCounter()
 
-    var juiceCount by remember { mutableStateOf(0) }
-
-    StatelessCounter(waterCount, { waterCount++ })
-    StatelessCounter(juiceCount, { juiceCount++ })
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
+    }
 }
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
