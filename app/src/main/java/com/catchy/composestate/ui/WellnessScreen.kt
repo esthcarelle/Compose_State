3b6a5711b109ac0feeeed0e7d949b74dd9c5563e
carrelle <esthcarelle@gmail.com>
2023-10-29 21:34:46 +0200

    Removed the function that was getting the list

--- app/src/main/java/com/catchy/composestate/ui/WellnessScreen.kt
+++ app/src/main/java/com/catchy/composestate/ui/WellnessScreen.kt
@@ -2,8 +2,6 @@ package com.catchy.composestate.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -22,4 +20,3 @@ fun WellnessScreen(
             onCloseTask = { task -> wellnessViewModel.remove(task) })
     }
 }
-private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
