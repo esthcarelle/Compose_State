da94d5851a673d6cbd196e9d48164bce2f3fb2b5
carrelle <esthcarelle@gmail.com>
2023-10-30 11:47:58 +0200

    Removed unused imports and composables

--- app/src/main/java/com/catchy/composestate/MainActivity.kt
+++ app/src/main/java/com/catchy/composestate/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.catchy.composestate.ui.WellnessScreen
