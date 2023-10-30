3f5596396d4324b1d867db901c40f2b8cef83123
carrelle <esthcarelle@gmail.com>
2023-10-30 11:45:45 +0200

    Removed unused imports and composables

--- app/src/main/java/com/catchy/composestate/MainActivity.kt
+++ app/src/main/java/com/catchy/composestate/MainActivity.kt
@@ -5,16 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.catchy.composestate.ui.WellnessScreen
 import com.catchy.composestate.ui.WellnessViewModel
@@ -40,23 +34,6 @@ fun HelloWorld(name: String, modifier: Modifier,numberOfKids: Int){
 fun TextButton(text: String) {
     Box(modifier = Modifier.clickable(onClick = {  })) {
         Text(text = text)
-    }
-}
-
-@Composable
-fun ImageButton() {
-    Box(modifier = Modifier.clickable(onClick = { })) {
-        Icon(painterResource(id = R.drawable.ic_launcher_background), contentDescription = "")
-    }
-}
-
-@Composable
-fun TextImageButton(text: String) {
-    Box(modifier = Modifier.clickable(onClick = {  })) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Icon(painterResource(id = R.drawable.ic_launcher_background), contentDescription = "")
-            Text(text = text)
-        }
     }
 }
 
