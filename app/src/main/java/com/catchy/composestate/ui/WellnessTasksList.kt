db92be628624d1dd3e94f41c9d3a0e483984dfa1
carrelle <esthcarelle@gmail.com>
2023-10-30 11:39:56 +0200

    Added an initial checked state

--- app/src/main/java/com/catchy/composestate/ui/WellnessTasksList.kt
+++ app/src/main/java/com/catchy/composestate/ui/WellnessTasksList.kt
@@ -3,12 +3,14 @@ package com.catchy.composestate.ui
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessTasksList(
     list: List<WellnessTask>,
     onCloseTask: (WellnessTask) -> Unit,
+    onCheckedTask: (WellnessTask, Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
     LazyColumn(modifier = modifier) {
@@ -16,7 +18,10 @@ fun WellnessTasksList(
             items = list,
             key = { task -> task.id }
         ) { task ->
-            WellnessTaskItem(taskName = task.label, onClose = { onCloseTask(task) })
+            WellnessTaskItem(taskName = task.label,
+                checked = task.checked,
+                onCheckedChange = { checked -> onCheckedTask(task, checked) },
+                onClose = { onCloseTask(task) })
         }
     }
 }
